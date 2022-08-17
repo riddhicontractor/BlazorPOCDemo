1c20775f77a70d7d3a7d6d3970c012756045e79e
Riddhi Contractor <riddhi.contractor@zymr.com>
2022-08-17 13:11:29 +0530

    Till 17 Aug

---
+++ BlazorDemoPOC/Data/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemoPOC.Data
+{
+    public class UserInfo
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ BlazorDemoPOC/Data/UserService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemoPOC.Data
+{
+    public class UserService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //get all users
+        public List<UserInfo> GetUser()
+        {
+            var userList = _db.Users.ToList();
+            return userList;
+        }
+
+        //insert user info
+        public string Create(UserInfo objUser)
+        {
+            _db.Users.Add(objUser);
+            _db.SaveChanges();
+            return "User added successfully!";
+        }
+    }
+}
