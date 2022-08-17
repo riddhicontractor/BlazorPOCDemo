5e706b0d699a03aff267a3921005f05084dc8f0a
Riddhi Contractor <riddhi.contractor@zymr.com>
2022-08-17 19:10:14 +0530

    Till 17 Aug EOD

--- BlazorDemoPOC/Data/UserService.cs
+++ BlazorDemoPOC/Data/UserService.cs
@@ -21,12 +21,80 @@ namespace BlazorDemoPOC.Data
             return userList;
         }
 
+        public UserInfo GetUser(string id)
+        {
+            var user = (from u in _db.Users
+                       where u.UserId==Convert.ToInt32(id)
+                       select new UserInfo
+                       {
+                           UserId=u.UserId,
+                           FirstName = u.FirstName,
+                           LastName = u.LastName,
+                           Gender = u.Gender,
+                           Department = u.Department,
+                           Username = u.Username,
+                           Password = u.Password
+                       }).First();
+            return user;
+        }
         //insert user info
         public string Create(UserInfo objUser)
         {
             _db.Users.Add(objUser);
             _db.SaveChanges();
             return "User added successfully!";
+        }
+
+        public string Edit(UserInfo objUser)
+        {
+            var user = _db.Users.SingleOrDefault(a => a.UserId == objUser.UserId);
+            if(user != null)
+            {
+                user.FirstName = objUser.FirstName;
+                user.LastName = objUser.LastName;
+                user.Gender = objUser.Gender;
+                user.Department = objUser.Department;
+                user.Username = objUser.Username;
+                user.Password = objUser.Password;
+               // _db.Users.Update(user);
+                _db.Entry(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _db.SaveChanges();
+               
+            }
+            return "User updated successfully!";
+        }
+
+        //delete user
+        public string Delete(UserInfo objUser)
+        {
+            var user = _db.Users.Find(objUser.UserId);
+            if(user != null)
+            {
+                _db.Users.Remove(user);
+                _db.SaveChanges();
+            }
+            return "User deleted successfully!";
+        }
+
+        //login user
+        public bool UserLogin(UserInfo userInfo)
+        {
+            try
+            {
+                var query = from u in _db.Users
+                            where userInfo.Username == u.Username &&
+                            userInfo.Password == u.Password
+                            select u;
+                if (query.FirstOrDefault() != null)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
